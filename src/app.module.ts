279bc5cc22d065dbdfca73f6ff91db3bebf0ca91
Mohammadi <sina@mohammadi.dev>
2025-07-13 23:07:07 +0330

    feat: add GraphQL support to the application
    - Modified app.module.ts to import and configure ApolloDriver and GraphQLModule for GraphQL support.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,12 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
-@Module({ imports: [ConfigModule.forRoot({ isGlobal: true })] })
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver, autoSchemaFile: true, graphiql: true }),
+  ],
+})
 export class AppModule {}
