19f7ef964ef95ea78f1754047950f6277cec919c
Mohammadi <sina@mohammadi.dev>
2025-07-18 20:51:40 +0330

    feat(common): add common module with prisma service
    - Introduce a new CommonModule that provides and exports PrismaService for database access. The service automatically connects to the database on module initialization.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver, autoSchemaFile: true, graphiql: true }),
+    CommonModule,
   ],
 })
 export class AppModule {}
